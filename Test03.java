30d39f7f9a554006b9037ce3cf00f78419819ce9
zhangyx2527 <252703218@qq.com>
2024-11-22 10:34:39 +0800

    01用户初始化文件

---
+++ Test03.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhangyuxuan
+ * @create 2024-11-22 10:32
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("01用户初始化文件");
+    }
+}
